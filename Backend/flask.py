11bfbf297251abf815d3025b6a65a208867a0b3c
Jacky Feng <jfeng14@students.cs.ubc.ca>
2024-01-20 20:45:32 -0800

    addedprompt_function

--- Backend/flask.py
+++ Backend/flask.py
@@ -4,16 +4,19 @@ from IPython.display import Image, display
 import flask
 import json
 import ai
+import imageprompt
 from flask_cors import CORS
 
 app = Flask('backend')
 CORS(app)
 
-prompt = "[insert prompt]"
-
 @app.route('/data', methods=["GET"])
-def users():
-    return ai.imagenerator(prompt)
+
+def getprompt():
+      prompt = imageprompt.prompt
+
+def imageretrival():
+        return ai.imagenerator(prompt)
 
 if __name__ == "__main__":
     app.run("localhost", 6969)
